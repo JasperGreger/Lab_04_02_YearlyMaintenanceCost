c8db8a0fd03c614f60944292938959f47bb7b90f
Jasper <jasper@Jaspers-MacBook-Pro.local>
2026-09-17 13:20:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    double spring = 120.75;
+    double summer = 190.95;
+    double fall = 130.25;
+    double winter = 170.55;
+    double total = 0;
+
+    total = spring + summer + fall + winter;
+
+    IO.println("The total cost for the full years is " + total);
+
+}
